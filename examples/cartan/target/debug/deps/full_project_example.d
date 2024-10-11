4eda18bf605421a985383bb87c011ad71c51499f
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-10-12 00:49:36 +0300

    Clean up example project target from git

--- examples/cartan/target/debug/deps/full_project_example.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\Programming\Rust\reast\examples\full_project_example\target\debug\deps\full_project_example.exe: main.rs
-
-D:\Projects\Programming\Rust\reast\examples\full_project_example\target\debug\deps\full_project_example.d: main.rs
-
-main.rs:
