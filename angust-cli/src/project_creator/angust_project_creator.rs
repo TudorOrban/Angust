6d5b147413755d77f230369804c459fc8bbe8865
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-10-12 18:15:39 +0300

    Continue fleshing out create project command in CLI tool

--- angust-cli/src/project_creator/angust_project_creator.rs
+++ angust-cli/src/project_creator/angust_project_creator.rs
@@ -1,5 +1,9 @@
 use std::{env, fs, path::PathBuf, process::Command};
 
+use toml_edit::{value, DocumentMut};
+
+use crate::project_creator::utils::string_pascal_to_snake_case;
+
 use super::types;
 
 
@@ -7,20 +11,23 @@ pub fn create_project(name: &str) {
     println!("Creating a new Angust project with name: {}", name);
 
     let current_dir = env::current_dir().expect("Failed to get current directory");
-    let project_root_path = current_dir.join(name);
+    let snake_case_name = string_pascal_to_snake_case(name);
+    let project_root_path = current_dir.join(snake_case_name.clone());
 
-    let is_success = create_root_directory(&project_root_path, name);
+    let is_success = create_root_directory(&project_root_path, &snake_case_name);
     if !is_success {
         return;
     }
 
     initialize_cargo_project(&project_root_path);
 
+    adjust_cargo_toml(&project_root_path, &snake_case_name);
+
     create_angust_configuration_file(&project_root_path);
 
 }
 
-fn create_root_directory(project_root_path: &PathBuf, name: &str) -> bool {
+fn create_root_directory(project_root_path: &PathBuf, name: &String) -> bool {
     if project_root_path.exists() {
         if project_root_path.is_dir() {
             eprintln!("A directory with the name '{}' already exists.", name);
@@ -45,7 +52,7 @@ fn create_root_directory(project_root_path: &PathBuf, name: &str) -> bool {
 
 fn initialize_cargo_project(project_root_path: &PathBuf) {
     match Command::new("cargo").arg("--version").output() {
-        Ok(output) => println!("Found Cargo: {}", String::from_utf8_lossy(&output.stdout)),
+        Ok(_) => {}
         Err(_) => {
             eprintln!("Cargo is not installed or not found in PATH.");
             return;
@@ -64,6 +71,31 @@ fn initialize_cargo_project(project_root_path: &PathBuf) {
     } else {
         eprintln!("Failed to initialize cargo project.");
     }
+}
+
+fn adjust_cargo_toml(project_root_path: &PathBuf, name: &String) {
+    let cargo_toml_path = project_root_path.join("Cargo.toml");
+
+    let cargo_toml_contents = fs::read_to_string(&cargo_toml_path)
+        .expect("Failed to read Cargo.toml");
+    let mut doc = cargo_toml_contents.parse::<DocumentMut>()
+        .expect("Failed to parse Cargo.toml");
+
+    // Modify the package name and version
+    doc["package"]["name"] = value(name);
+    doc["package"]["version"] = value("0.1.0");
+    doc["package"]["edition"] = value("2021");
+
+    // Specify dependency to be a local path for development purposes
+    doc["dependencies"]["angust"] = value("../../../angust-core/");
+
+    // Define a binary target
+    let bin_table = doc["bin"].or_insert(toml_edit::table());
+    bin_table["name"] = value(name);
+    bin_table["path"] = value("src/main.rs");
+
+    fs::write(&cargo_toml_path, doc.to_string())
+        .expect("Failed to write modified Cargo.toml");
 }
 
 fn create_angust_configuration_file(project_root_path: &PathBuf) {
--- angust-cli/src/project_creator/mod.rs
+++ angust-cli/src/project_creator/mod.rs
@@ -1,2 +1,3 @@
 pub mod angust_project_creator;
 pub mod types;
+mod utils;
