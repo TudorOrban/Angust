1f27d9cfcf4d16840b0e9a3a52a2b69e2d60d04a
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-10-21 19:12:23 +0300

    Start introducing Lifetime issues solution throughout codebase

--- angust-core/src/parsing/expression/ast_evaluator.rs
+++ angust-core/src/parsing/expression/ast_evaluator.rs
@@ -1,12 +1,12 @@
-use std::{any::Any, cell::RefCell, collections::HashMap, rc::Rc};
+use std::any::Any;
 
-use crate::rendering::elements::component::{component_state::{get_nested_field, get_nested_field_new, ReactiveState, ReflectiveState, ReflectiveStateNew}, functions::component_functions::ComponentFunctions};
+use crate::rendering::elements::component::{component_state::{get_nested_field, ReactiveState}, functions::component_functions::ComponentFunctions};
 
 use super::ast::{ASTNode, Operator};
 
 
 // TODO: Fix Box leaks with thread_local! storage
-pub fn evaluate_ast_new<State: ReflectiveStateNew>(
+pub fn evaluate_ast<State: ReactiveState>(
     node: &ASTNode,
     state: &State,
     functions: &ComponentFunctions<State>,
@@ -18,80 +18,54 @@ pub fn evaluate_ast_new<State: ReflectiveStateNew>(
         ASTNode::StringLiteral(string) => {
             Ok(Box::new(string.clone()))
         },
+        ASTNode::Boolean(boolean) => {
+            Ok(Box::new(*boolean))
+        },
         ASTNode::Identifier(name) => {
-            match get_nested_field_new(state, &[name]) {
+            match get_nested_field(state, &[name]) {
                 Some(val) => {
                     Ok(Box::new(val.as_any()))
                 }
                 None => Err(format!("Field {} not found", name)),
             }
         },
+        ASTNode::FunctionCall(name, args) => {
+            // evaluate_component_function_new(name, args.clone(), state, functions)
+            Err("Function calls are not supported yet".to_string())
+        },
+        ASTNode::BinaryOperation { operator, left, right } =>
+            evaluate_binary_operation_new(operator, left, right, state, functions),
+        ASTNode::Comparison { operator, left, right } =>
+            evaluate_comparison_new(operator, left, right, state, functions),
+        ASTNode::LogicalOperation { operator, left, right } =>
+            evaluate_logical_operation_new(operator, left, right, state, functions),
         _ => Err("Only identifiers are supported for now".to_string()),
     }
 }
 
-pub fn evaluate_ast<'a, State: ReactiveState>(
-    node: &ASTNode,
-    state: &'a State,
-    functions: &ComponentFunctions<State>,
-) -> Result<&'a dyn Any, String> {
-    match node {
-        ASTNode::Number(num) => {
-            let leaked_value = Box::leak(Box::new(*num) as Box<dyn Any>);
-            Ok(leaked_value as &'a dyn Any)
-        },
-        ASTNode::StringLiteral(string) => {
-            let leaked_value = Box::leak(Box::new(string.clone()) as Box<dyn Any>);
-            Ok(leaked_value as &'a dyn Any)
-        },
-        ASTNode::Boolean(boolean) => {
-            let leaked_value = Box::leak(Box::new(*boolean) as Box<dyn Any>);
-            Ok(leaked_value as &'a dyn Any)
-        },
-        ASTNode::Identifier(name) => {
-            match get_nested_field(state, &[name]) {
-                Some(val) => {
-                    Ok(val.as_any())
-                }
-                None => Err(format!("Field {} not found", name)),
-            }
-        },
-        ASTNode::FunctionCall(name, args) => {
-            evaluate_component_function(name, args.clone(), state, functions)
-        },
-        ASTNode::BinaryOperation { operator, left, right } =>
-            evaluate_binary_operation(operator, left, right, state, functions),
-        ASTNode::Comparison { operator, left, right } =>
-            evaluate_comparison(operator, left, right, state, functions),
-        ASTNode::LogicalOperation { operator, left, right } =>
-            evaluate_logical_operation(operator, left, right, state, functions),
-    }
-}
+// fn evaluate_component_function_new<State: ReactiveState>(
+//     name: &str,
+//     args: Vec<ASTNode>,
+//     state: &State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<Box<dyn Any>, String> {
+//     let arg_values: Result<Vec<Box<dyn Any>>, String> = args.iter()
+//         .map(|arg| evaluate_ast(arg, state, functions))
+//         .collect();
 
+//     match functions.dynamic_params_functions.get(name) {
+//         Some(func) => Ok(func(state, arg_values?)),
+//         None => Err(format!("Function {} not found in component functions", name)),
+//     }
+// }
 
-fn evaluate_component_function<'a, State: ReactiveState>(
-    name: &str,
-    args: Vec<ASTNode>,
-    state: &'a State,
-    functions: &ComponentFunctions<State>,
-) -> Result<&'a dyn Any, String> {
-    let arg_values: Result<Vec<&dyn Any>, String> = args.iter()
-        .map(|arg| evaluate_ast(arg, state, functions))
-        .collect();
-
-    match functions.dynamic_params_functions.get(name) {
-        Some(func) => Ok(func(state, arg_values?)),
-        None => Err(format!("Function {} not found in component functions", name)),
-    }
-}
-
-fn evaluate_binary_operation<'a, State: ReactiveState>(
+fn evaluate_binary_operation_new<State: ReactiveState>(
     operator: &Operator,
     left: &ASTNode,
     right: &ASTNode,
-    state: &'a State,
+    state: &State,
     functions: &ComponentFunctions<State>,
-) -> Result<&'a dyn Any, String> {
+) -> Result<Box<dyn Any>, String> {
     let left_val = evaluate_ast(left, state, functions)?;
     let right_val = evaluate_ast(right, state, functions)?;
 
@@ -106,34 +80,34 @@ fn evaluate_binary_operation<'a, State: ReactiveState>(
         _ => return Err("Unsupported operation for binary operation".to_string()),
     };
 
-    Ok(Box::leak(Box::new(result)) as &'a dyn Any) 
+    Ok(Box::new(result)) 
 }
 
-fn evaluate_comparison<'a, State: ReactiveState>(
+fn evaluate_comparison_new<State: ReactiveState>(
     operator: &Operator,
     left: &ASTNode,
     right: &ASTNode,
-    state: &'a State,
+    state: &State,
     functions: &ComponentFunctions<State>,
-) -> Result<&'a dyn Any, String> {
+) -> Result<Box<dyn Any>, String> {
     let left_val = evaluate_ast(left, state, functions)?;
     let right_val = evaluate_ast(right, state, functions)?;
 
-    if let Some(result) = try_numeric_comparison(operator, left_val, right_val)? {
-        return Ok(Box::leak(Box::new(result)) as &dyn Any);  
+    if let Some(result) = try_numeric_comparison_new(operator, left_val, right_val)? {
+        return Ok(Box::new(result));  
     }
 
-    if let Some(result) = try_string_comparison(operator, left_val, right_val)? {
-        return Ok(Box::leak(Box::new(result)) as &dyn Any);
-    }
+    // if let Some(result) = try_string_comparison_new(operator, left_val, right_val)? {
+    //     return Ok(Box::new(result));
+    // }
 
     Err("Type mismatch or unsupported comparison type".to_string())
 }
 
-fn try_numeric_comparison<'a>(
+fn try_numeric_comparison_new(
     operator: &Operator,
-    left_val: &'a dyn Any,
-    right_val: &'a dyn Any,
+    left_val: Box<dyn Any>,
+    right_val: Box<dyn Any>,
 ) -> Result<Option<bool>, String> {
     if let (Some(left_float), Some(right_float)) = (
         left_val.downcast_ref::<f64>(),
@@ -154,10 +128,10 @@ fn try_numeric_comparison<'a>(
     Ok(None)
 }
 
-fn try_string_comparison<'a>(
+fn try_string_comparison_new(
     operator: &Operator,
-    left_val: &'a dyn Any,
-    right_val: &'a dyn Any,
+    left_val: Box<dyn Any>,
+    right_val: Box<dyn Any>,
 ) -> Result<Option<bool>, String> {
     if let (Some(left_str), Some(right_str)) = (
         left_val.downcast_ref::<String>(),
@@ -174,14 +148,13 @@ fn try_string_comparison<'a>(
     Ok(None)
 }
 
-
-fn evaluate_logical_operation<'a, State: ReactiveState>(
+fn evaluate_logical_operation_new<State: ReactiveState>(
     operator: &Operator,
     left: &ASTNode,
     right: &ASTNode,
-    state: &'a State,
+    state: &State,
     functions: &ComponentFunctions<State>,
-) -> Result<&'a dyn Any, String> {
+) -> Result<Box<dyn Any>, String> {
     let left_val = evaluate_ast(left, state, functions)?;
     let right_val = evaluate_ast(right, state, functions)?;
 
@@ -194,5 +167,172 @@ fn evaluate_logical_operation<'a, State: ReactiveState>(
         _ => return Err("Unsupported operation for logical operation".to_string()),
     };
 
-    Ok(Box::leak(Box::new(result)) as &'a dyn Any) 
+    Ok(Box::new(result)) 
 }
+
+// pub fn evaluate_ast<'a, State: ReactiveState>(
+//     node: &ASTNode,
+//     state: &'a State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<&'a dyn Any, String> {
+//     match node {
+//         ASTNode::Number(num) => {
+//             let leaked_value = Box::leak(Box::new(*num) as Box<dyn Any>);
+//             Ok(leaked_value as &'a dyn Any)
+//         },
+//         ASTNode::StringLiteral(string) => {
+//             let leaked_value = Box::leak(Box::new(string.clone()) as Box<dyn Any>);
+//             Ok(leaked_value as &'a dyn Any)
+//         },
+//         ASTNode::Boolean(boolean) => {
+//             let leaked_value = Box::leak(Box::new(*boolean) as Box<dyn Any>);
+//             Ok(leaked_value as &'a dyn Any)
+//         },
+//         ASTNode::Identifier(name) => {
+//             match get_nested_field(state, &[name]) {
+//                 Some(val) => {
+//                     Ok(val.as_any())
+//                 }
+//                 None => Err(format!("Field {} not found", name)),
+//             }
+//         },
+//         ASTNode::FunctionCall(name, args) => {
+//             evaluate_component_function(name, args.clone(), state, functions)
+//         },
+//         ASTNode::BinaryOperation { operator, left, right } =>
+//             evaluate_binary_operation(operator, left, right, state, functions),
+//         ASTNode::Comparison { operator, left, right } =>
+//             evaluate_comparison(operator, left, right, state, functions),
+//         ASTNode::LogicalOperation { operator, left, right } =>
+//             evaluate_logical_operation(operator, left, right, state, functions),
+//     }
+// }
+
+
+// fn evaluate_component_function<'a, State: ReactiveState>(
+//     name: &str,
+//     args: Vec<ASTNode>,
+//     state: &'a State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<&'a dyn Any, String> {
+//     let arg_values: Result<Vec<&dyn Any>, String> = args.iter()
+//         .map(|arg| evaluate_ast(arg, state, functions))
+//         .collect();
+
+//     match functions.dynamic_params_functions.get(name) {
+//         Some(func) => Ok(func(state, arg_values?)),
+//         None => Err(format!("Function {} not found in component functions", name)),
+//     }
+// }
+
+// fn evaluate_binary_operation<'a, State: ReactiveState>(
+//     operator: &Operator,
+//     left: &ASTNode,
+//     right: &ASTNode,
+//     state: &'a State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<&'a dyn Any, String> {
+//     let left_val = evaluate_ast(left, state, functions)?;
+//     let right_val = evaluate_ast(right, state, functions)?;
+
+//     let left_float = *left_val.downcast_ref::<f64>().ok_or("Type mismatch")?;
+//     let right_float = *right_val.downcast_ref::<f64>().ok_or("Type mismatch")?;
+
+//     let result = match operator {
+//         Operator::Add => left_float + right_float,
+//         Operator::Subtract => left_float - right_float,
+//         Operator::Multiply => left_float * right_float,
+//         Operator::Divide => left_float / right_float,
+//         _ => return Err("Unsupported operation for binary operation".to_string()),
+//     };
+
+//     Ok(Box::leak(Box::new(result)) as &'a dyn Any) 
+// }
+
+// fn evaluate_comparison<'a, State: ReactiveState>(
+//     operator: &Operator,
+//     left: &ASTNode,
+//     right: &ASTNode,
+//     state: &'a State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<&'a dyn Any, String> {
+//     let left_val = evaluate_ast(left, state, functions)?;
+//     let right_val = evaluate_ast(right, state, functions)?;
+
+//     if let Some(result) = try_numeric_comparison(operator, left_val, right_val)? {
+//         return Ok(Box::leak(Box::new(result)) as &dyn Any);  
+//     }
+
+//     if let Some(result) = try_string_comparison(operator, left_val, right_val)? {
+//         return Ok(Box::leak(Box::new(result)) as &dyn Any);
+//     }
+
+//     Err("Type mismatch or unsupported comparison type".to_string())
+// }
+
+// fn try_numeric_comparison<'a>(
+//     operator: &Operator,
+//     left_val: &'a dyn Any,
+//     right_val: &'a dyn Any,
+// ) -> Result<Option<bool>, String> {
+//     if let (Some(left_float), Some(right_float)) = (
+//         left_val.downcast_ref::<f64>(),
+//         right_val.downcast_ref::<f64>(),
+//     ) {
+//         let result = match operator {
+//             Operator::Equal => left_float == right_float,
+//             Operator::NotEqual => left_float != right_float,
+//             Operator::Less => left_float < right_float,
+//             Operator::Greater => left_float > right_float,
+//             Operator::LessEqual => left_float <= right_float,
+//             Operator::GreaterEqual => left_float >= right_float,
+//             _ => return Err("Unsupported operator for numeric comparison".to_string()),
+//         };
+
+//         return Ok(Some(result));
+//     }
+//     Ok(None)
+// }
+
+// fn try_string_comparison<'a>(
+//     operator: &Operator,
+//     left_val: &'a dyn Any,
+//     right_val: &'a dyn Any,
+// ) -> Result<Option<bool>, String> {
+//     if let (Some(left_str), Some(right_str)) = (
+//         left_val.downcast_ref::<String>(),
+//         right_val.downcast_ref::<String>(),
+//     ) {
+//         let result = match operator {
+//             Operator::Equal => left_str == right_str,
+//             Operator::NotEqual => left_str != right_str,
+//             _ => return Err("String comparison only supports Equal and NotEqual".to_string()),
+//         };
+
+//         return Ok(Some(result));
+//     }
+//     Ok(None)
+// }
+
+
+// fn evaluate_logical_operation<'a, State: ReactiveState>(
+//     operator: &Operator,
+//     left: &ASTNode,
+//     right: &ASTNode,
+//     state: &'a State,
+//     functions: &ComponentFunctions<State>,
+// ) -> Result<&'a dyn Any, String> {
+//     let left_val = evaluate_ast(left, state, functions)?;
+//     let right_val = evaluate_ast(right, state, functions)?;
+
+//     let left_bool = *left_val.downcast_ref::<bool>().ok_or("Type mismatch")?;
+//     let right_bool = *right_val.downcast_ref::<bool>().ok_or("Type mismatch")?;
+
+//     let result = match operator {
+//         Operator::And => left_bool && right_bool,
+//         Operator::Or => left_bool || right_bool,
+//         _ => return Err("Unsupported operation for logical operation".to_string()),
+//     };
+
+//     Ok(Box::leak(Box::new(result)) as &'a dyn Any) 
+// }
--- angust-core/src/rendering/elements/component/component_state.rs
+++ angust-core/src/rendering/elements/component/component_state.rs
@@ -1,27 +1,26 @@
-use std::{any::Any, rc::Rc};
+use std::any::Any;
 
 use super::reactivity::ComponentEvent;
 
-// New
-pub trait ReflectiveStateNew {
-    fn get_field(&self, name: &str) -> Option<Box<dyn ReflectiveStateNew>>;
+pub trait ReflectiveState {
+    fn get_field(&self, name: &str) -> Option<Box<dyn ReflectiveState>>;
     fn set_field(&mut self, name: &str, value: Box<dyn Any>);
     fn get_all_properties(&self) -> Vec<&str>;
     fn as_any(&self) -> Box<dyn Any>; 
-    fn clone_box(&self) -> Box<dyn ReflectiveStateNew>;
+    fn clone_box(&self) -> Box<dyn ReflectiveState>;
 }
 
-impl Clone for Box<dyn ReflectiveStateNew> {
-    fn clone(&self) -> Box<dyn ReflectiveStateNew> {
+impl Clone for Box<dyn ReflectiveState> {
+    fn clone(&self) -> Box<dyn ReflectiveState> {
         self.clone_box()
     }
 }
 
-pub fn get_nested_field_new(
-    obj: &dyn ReflectiveStateNew, 
+pub fn get_nested_field(
+    obj: &dyn ReflectiveState, 
     path: &[&str]
-) -> Option<Box<dyn ReflectiveStateNew>> {
-    let mut current: Box<dyn ReflectiveStateNew> = obj.clone_box(); 
+) -> Option<Box<dyn ReflectiveState>> {
+    let mut current: Box<dyn ReflectiveState> = obj.clone_box(); 
 
     for &field in path {
         current = current.get_field(field)?;
@@ -29,10 +28,37 @@ pub fn get_nested_field_new(
     Some(current)
 }
 
+// Implementations
+pub struct NoState;
 
+impl ReflectiveState for NoState {
+    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveState>> {
+        None
+    }
 
-impl ReflectiveStateNew for String {
-    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveStateNew>> {
+    fn set_field(&mut self, _name: &str, _value: Box<dyn Any>) {}
+
+    fn get_all_properties(&self) -> Vec<&str> {
+        vec![]
+    }
+
+    fn as_any(&self) -> Box<dyn Any> {
+        Box::new(self.clone())
+    }
+
+    fn clone_box(&self) -> Box<dyn ReflectiveState> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for NoState {
+    fn clone(&self) -> Self {
+        NoState {}
+    }
+}
+
+impl ReflectiveState for String {
+    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveState>> {
         None
     }
 
@@ -48,22 +74,87 @@ impl ReflectiveStateNew for String {
         Box::new(self.clone())
     }
 
-    fn clone_box(&self) -> Box<dyn ReflectiveStateNew> {
+    fn clone_box(&self) -> Box<dyn ReflectiveState> {
+        Box::new(self.clone())
+    }
+}
+
+impl ReflectiveState for u32 {
+    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveState>> {
+        None
+    }
+
+    fn set_field(&mut self, _name: &str, _value: Box<dyn Any>) {
+        // Do nothing
+    }
+
+    fn get_all_properties(&self) -> Vec<&str> {
+        vec![]
+    }
+
+    fn as_any(&self) -> Box<dyn Any> {
+        Box::new(self.clone())
+    }
+
+    fn clone_box(&self) -> Box<dyn ReflectiveState> {
+        Box::new(self.clone())
+    }
+}
+
+impl ReflectiveState for u8 {
+    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveState>> {
+        None
+    }
+
+    fn set_field(&mut self, _name: &str, _value: Box<dyn Any>) {
+        // Do nothing
+    }
+
+    fn get_all_properties(&self) -> Vec<&str> {
+        vec![]
+    }
+
+    fn as_any(&self) -> Box<dyn Any> {
+        Box::new(self.clone())
+    }
+
+    fn clone_box(&self) -> Box<dyn ReflectiveState> {
+        Box::new(self.clone())
+    }
+}
+
+impl ReflectiveState for f64 {
+    fn get_field(&self, _name: &str) -> Option<Box<dyn ReflectiveState>> {
+        None
+    }
+
+    fn set_field(&mut self, _name: &str, _value: Box<dyn Any>) {
+        // Do nothing
+    }
+    
+    fn get_all_properties(&self) -> Vec<&str> {
+        vec![]
+    }
+    
+    fn as_any(&self) -> Box<dyn Any> {
+        Box::new(self.clone())
+    }
+
+    fn clone_box(&self) -> Box<dyn ReflectiveState> {
         Box::new(self.clone())
     }
 }
 
 
 
-
 // Old
 // Core traits of the component state, enabling (nested) reflection and reactivity respectively
-pub trait ReflectiveState {
-    fn get_field(&self, name: &str) -> Option<&dyn ReflectiveState>;
-    fn set_field(&mut self, name: &str, value: Box<dyn Any>);
-    fn get_all_properties(&self) -> Vec<&str>;
-    fn as_any(&self) -> &dyn Any;
-}
+// pub trait ReflectiveState {
+//     fn get_field(&self, name: &str) -> Option<&dyn ReflectiveState>;
+//     fn set_field(&mut self, name: &str, value: Box<dyn Any>);
+//     fn get_all_properties(&self) -> Vec<&str>;
+//     fn as_any(&self) -> &dyn Any;
+// }
 
 pub trait ReactiveState : ReflectiveState {
     fn subscribe_to_property<F>(&mut self, property_name: &str, callback: F)
@@ -72,112 +163,112 @@ pub trait ReactiveState : ReflectiveState {
 }
 
 // Nested reflection
-pub fn get_nested_field<'a>(
-    obj: &'a dyn ReflectiveState, 
-    path: &[&str]
-) -> Option<&'a dyn ReflectiveState> {
-    let mut current = obj;
-    for &field in path {
-        current = current.get_field(field)?;
-    }
-    Some(current)
-}
+// pub fn get_nested_field<'a>(
+//     obj: &'a dyn ReflectiveState, 
+//     path: &[&str]
+// ) -> Option<&'a dyn ReflectiveState> {
+//     let mut current = obj;
+//     for &field in path {
+//         current = current.get_field(field)?;
+//     }
+//     Some(current)
+// }
 
-pub struct NoState;
+// pub struct NoState;
 
-impl ReflectiveState for NoState {
-    fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
-        None
-    }
+// impl ReflectiveState for NoState {
+//     fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
+//         None
+//     }
 
-    fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {}
+//     fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {}
 
-    fn get_all_properties(&self) -> Vec<&str> {
-        vec![]
-    }
+//     fn get_all_properties(&self) -> Vec<&str> {
+//         vec![]
+//     }
 
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-}
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self
+//     }
+// }
 
-impl ReactiveState for NoState {
+// impl ReactiveState for NoState {
 
-    fn subscribe_to_property<F>(&mut self, _property_name: &str, _callback: F)
-    where
-        F: 'static + FnMut(&crate::rendering::elements::component::reactivity::ComponentEvent),
-    {
-    }
-}
+//     fn subscribe_to_property<F>(&mut self, _property_name: &str, _callback: F)
+//     where
+//         F: 'static + FnMut(&crate::rendering::elements::component::reactivity::ComponentEvent),
+//     {
+//     }
+// }
 
-impl ReflectiveState for String {
-    fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
-        None
-    }
+// impl ReflectiveState for String {
+//     fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
+//         None
+//     }
 
-    fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
-        // Do nothing
-    }
+//     fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
+//         // Do nothing
+//     }
 
-    fn get_all_properties(&self) -> Vec<&str> {
-        vec![]
-    }
+//     fn get_all_properties(&self) -> Vec<&str> {
+//         vec![]
+//     }
 
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-}
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self
+//     }
+// }
 
-impl ReflectiveState for u32 {
-    fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
-        None
-    }
+// impl ReflectiveState for u32 {
+//     fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
+//         None
+//     }
 
-    fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
-        // Do nothing
-    }
+//     fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
+//         // Do nothing
+//     }
 
-    fn get_all_properties(&self) -> Vec<&str> {
-        vec![]
-    }
+//     fn get_all_properties(&self) -> Vec<&str> {
+//         vec![]
+//     }
 
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-}
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self
+//     }
+// }
 
-impl ReflectiveState for u8 {
-    fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
-        None
-    }
+// impl ReflectiveState for u8 {
+//     fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
+//         None
+//     }
 
-    fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
-        // Do nothing
-    }
+//     fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
+//         // Do nothing
+//     }
 
-    fn get_all_properties(&self) -> Vec<&str> {
-        vec![]
-    }
+//     fn get_all_properties(&self) -> Vec<&str> {
+//         vec![]
+//     }
 
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-}
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self
+//     }
+// }
 
-impl ReflectiveState for f64 {
-    fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
-        None
-    }
+// impl ReflectiveState for f64 {
+//     fn get_field(&self, _name: &str) -> Option<&dyn ReflectiveState> {
+//         None
+//     }
 
-    fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
-        // Do nothing
-    }
+//     fn set_field(&mut self, _name: &str, _value: Box<dyn std::any::Any>) {
+//         // Do nothing
+//     }
     
-    fn get_all_properties(&self) -> Vec<&str> {
-        vec![]
-    }
+//     fn get_all_properties(&self) -> Vec<&str> {
+//         vec![]
+//     }
     
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-}
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self
+//     }
+// }
