99e9f6a74b564e2f7e4122af50117d9ffe2bdb06
Tudor Andrei Orban <130213626+TudorOrban@users.noreply.github.com>
2024-11-06 15:12:55 +0200

    Document layout system

--- angust-core/src/rendering/layout/mod.rs
+++ angust-core/src/rendering/layout/mod.rs
@@ -1,2 +1,9 @@
+/*
+ * Layout system, consisting of two stages:
+ *   a. A leaves -> root traversal of the DOM, estimating sizes of the elements
+ *   b. A root -> leaves pass that recursively allocates positions and sizes,
+ * based on the estimated children sizes and parent constraints.
+ */
+
 pub mod space_allocation_system;
 pub mod size_estimation_system;
